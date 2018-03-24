71e3e92f9dcc580e9888db19f9c280e5bd35b602
qixuxiang <qixuxiang@outlook.com>
2018-03-24 16:27:41 +0800

    add python3 demo

---
+++ demo.py
@@ -0,0 +1,257 @@
+import os
+import tarfile
+import tempfile
+
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+import tensorflow as tf
+
+current_dir = os.getcwd()
+origin_img = Image.open('1311870427.731118.png')
+origin_width, origin_height = origin_img.size
+dir_name = "rgb2"
+isExists = os.path.exists(dir_name)
+if not isExists:
+    os.makedirs("dir_name")
+
+class DeepLabModel(object):
+  """Class to load deeplab model and run inference."""
+
+  INPUT_TENSOR_NAME = 'ImageTensor:0'
+  OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+  INPUT_SIZE = 513
+  FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+  def __init__(self, tarball_path):
+    """Creates and loads pretrained deeplab model."""
+    self.graph = tf.Graph()
+
+    graph_def = None
+    # Extract frozen graph from tar archive.
+    tar_file = tarfile.open(tarball_path)
+    for tar_info in tar_file.getmembers():
+      if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+        file_handle = tar_file.extractfile(tar_info)
+        graph_def = tf.GraphDef.FromString(file_handle.read())
+        break
+
+    tar_file.close()
+
+    if graph_def is None:
+      raise RuntimeError('Cannot find inference graph in tar archive.')
+
+    with self.graph.as_default():
+      tf.import_graph_def(graph_def, name='')
+
+    self.sess = tf.Session(graph=self.graph)
+
+  def run(self, image_name):
+    """Runs inference on a single image.
+
+    Args:
+      image: A PIL.Image object, raw input image.
+
+    Returns:
+      resized_image: RGB image resized from original input image.
+      seg_map: Segmentation map of `resized_image`.
+    """
+    #image_name = image_path.split("/")[-1]
+    image = Image.open(image_name)
+    width, height = image.size
+    resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+    target_size = (int(resize_ratio * width), int(resize_ratio * height))
+    resized_image = image.convert('RGB').resize(target_size, Image.ANTIALIAS)
+    batch_seg_map = self.sess.run(
+        self.OUTPUT_TENSOR_NAME,
+        feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+    seg_map = batch_seg_map[0]
+
+
+
+    #return resized_image, seg_map
+    plt.figure(figsize=(15, 5))
+    grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+    plt.subplot(grid_spec[0])
+    plt.imshow(resized_image)
+    plt.axis('off')
+    plt.title('input image')
+
+    plt.subplot(grid_spec[1])
+    seg_image = label_to_color_image(seg_map).astype(np.uint8)
+    #plt.imshow(seg_image)
+
+    im = Image.fromarray(seg_image)
+    im.resize(origin_img.size,Image.ANTIALIAS)
+    im.save(os.path.join('rgb2',image_name.split("/")[-1]), 'png')
+
+    plt.axis('off')
+
+    plt.title('segmentation map')
+
+    plt.subplot(grid_spec[2])
+    #plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis('off')
+    plt.title('segmentation overlay')
+
+    unique_labels = np.unique(seg_map)
+    ax = plt.subplot(grid_spec[3])
+    plt.imshow(
+      FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+    ax.yaxis.tick_right()
+    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+    plt.xticks([], [])
+    ax.tick_params(width=0.0)
+    plt.grid('off')
+    #plt.show()
+
+
+def create_pascal_label_colormap():
+  """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+
+  Returns:
+    A Colormap for visualizing segmentation results.
+  """
+  colormap = np.zeros((256, 3), dtype=int)
+  ind = np.arange(256, dtype=int)
+
+  for shift in reversed(range(8)):
+    for channel in range(3):
+      colormap[:, channel] |= ((ind >> channel) & 1) << shift
+    ind >>= 3
+
+  return colormap
+
+
+def label_to_color_image(label):
+  """Adds color defined by the dataset colormap to the label.
+
+  Args:
+    label: A 2D array with integer type, storing the segmentation label.
+
+  Returns:
+    result: A 2D array with floating type. The element of the array
+      is the color indexed by the corresponding element in the input label
+      to the PASCAL color map.
+
+  Raises:
+    ValueError: If label is not of rank 2 or its value is larger than color
+      map maximum entry.
+  """
+  if label.ndim != 2:
+    raise ValueError('Expect 2-D input label')
+
+  colormap = create_pascal_label_colormap()
+
+  if np.max(label) >= len(colormap):
+    raise ValueError('label value too large.')
+
+  return colormap[label]
+
+def count_files(path):
+    count = 0
+    for root , dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith(".png"):
+                count += 1
+    return count
+
+def files_to_list(path):
+    file_list = []
+    for root , dirs, files in os.walk(path):
+        for name in files:
+            file_list.append(name)
+
+    return file_list
+
+
+'''
+def vis_segmentation(image, seg_map):
+  """Visualizes input image, segmentation map and overlay view."""
+  plt.figure(figsize=(15, 5))
+  grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+  plt.subplot(grid_spec[0])
+  plt.imshow(image)
+  plt.axis('off')
+  plt.title('input image')
+
+  plt.subplot(grid_spec[1])
+  seg_image = label_to_color_image(seg_map).astype(np.uint8)
+  plt.imshow(seg_image)
+  im = Image.fromarray(seg_image)
+  im.resize(origin_img.size,Image.ANTIALIAS)
+  im.save('test.png', 'png')
+  plt.axis('off')
+
+  plt.title('segmentation map')
+
+  plt.subplot(grid_spec[2])
+  plt.imshow(image)
+  plt.imshow(seg_image, alpha=0.7)
+  plt.axis('off')
+  plt.title('segmentation overlay')
+
+  unique_labels = np.unique(seg_map)
+  ax = plt.subplot(grid_spec[3])
+  plt.imshow(
+      FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+  ax.yaxis.tick_right()
+  plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+  plt.xticks([], [])
+  ax.tick_params(width=0.0)
+  plt.grid('off')
+  plt.show()
+
+'''
+
+LABEL_NAMES = np.asarray([
+    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+])
+
+FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
+
+
+
+#@title Select and download models {display-mode: "form"}
+
+MODEL_NAME = 'mobilenetv2_coco_voctrainaug'  # @param ['mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval']
+
+model_dir = 'model'
+'''
+_MODEL_URLS = {
+    'mobilenetv2_coco_voctrainaug':
+        'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+    'mobilenetv2_coco_voctrainval':
+        'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
+    'xception_coco_voctrainaug':
+        'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
+    'xception_coco_voctrainval':
+        'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
+}
+_TARBALL_NAME = 'deeplab_model.tar.gz'
+
+'''
+
+download_path = os.path.join(model_dir, "deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz")
+
+MODEL = DeepLabModel(download_path)
+print('model loaded successfully!')
+
+
+
+
+
+if __name__ == '__main__':
+    file_lst = files_to_list('rgb')
+    cou = 0
+    for i in range(len(file_lst)):
+        if (i%50==0):
+            MODEL.run(os.path.join('rgb',file_lst[i]))
